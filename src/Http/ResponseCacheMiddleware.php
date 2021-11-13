cd3f6f15d3b4a75082bc3eeadb1accdd85ba038c
Florent Poujol <florent.poujol@freshmile.com>
2021-11-13 11:18:46 +0100

    Http: add a simplistic ResponseCacheMiddleware

---
+++ src/Http/ResponseCacheMiddleware.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FlorentPoujol\SmolFramework\Http;
+
+use FlorentPoujol\SmolFramework\Cache\CacheInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+final class ResponseCacheMiddleware
+{
+    public function __construct(
+        private CacheInterface $cache,
+    ) {
+    }
+
+    private string $cacheKey = '';
+
+    public function handleRequest(ServerRequestInterface $request): ?ResponseInterface
+    {
+        if ($request->getMethod() !== 'GET') {
+            return null;
+        }
+
+        $this->cacheKey = 'responses:' . md5(
+            $request->getMethod() .
+            $request->getHeader('host') .
+            $request->getUri() .
+            implode('', $request->getQueryParams())
+        );
+
+        return $this->cache->get($this->cacheKey);
+    }
+
+    public function handleResponse(ResponseInterface $response): void
+    {
+        if ($this->cacheKey === '' || $response->getStatusCode() !== 200) {
+            return;
+        }
+
+        $this->cache->set($this->cacheKey, $response, 60 * 5);
+    }
+}
