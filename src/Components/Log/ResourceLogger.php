d5e7301bbf6a59e88406ffc7b6325573116f4fe8
Florent Poujol <florent.poujol@freshmile.com>
2021-11-16 23:45:45 +0100

    Allow to pass a formatter callable to the ResourceLogger

--- src/Components/Log/ResourceLogger.php
+++ src/Components/Log/ResourceLogger.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace FlorentPoujol\SmolFramework\Components\Log;
 
-use FlorentPoujol\SmolFramework\Framework\Exceptions\SmolFrameworkException;
+use Exception;
 use Psr\Log\AbstractLogger;
 use Stringable;
 
@@ -13,8 +13,12 @@ final class ResourceLogger extends AbstractLogger
     /** @var null|resource */
     private $resource;
 
+    /**
+     * @param callable $formatter
+     */
     public function __construct(
-        private string $resourcePath
+        private string $resourcePath,
+        private $formatter = [$this, 'defaultLineFormatter']
     ) {
     }
 
@@ -22,10 +26,22 @@ final class ResourceLogger extends AbstractLogger
     {
         $resource = fopen($this->resourcePath, 'a+');
         if (! is_resource($resource)) {
-            throw new SmolFrameworkException("Can't create resource for path '$this->resourcePath'.");
+            throw new Exception("Can't create resource for path '$this->resourcePath'.");
         }
 
         $this->resource = $resource;
+    }
+
+    private function defaultLineFormatter(string $level, string $message, array $context): string
+    {
+        $date = date('Y-m-d H:i:s');
+        $line = "[$date] $level: $message";
+
+        if ($context !== []) {
+            $line .= ' ' . json_encode($context);
+        }
+
+        return $line;
     }
 
     /**
@@ -36,16 +52,12 @@ final class ResourceLogger extends AbstractLogger
     // @phpstan-ignore-next-line ($param context without type specified, but if we specify one, it complains it is not covariant
     public function log($level, Stringable|string $message, array $context = []): void
     {
-        $date = date('Y-m-d H:i:s');
-        $line = "[$date] $level: " . (string) $message;
-
-        if ($context !== []) {
-            $line .= ' ' . json_encode($context);
-        }
-
         if ($this->resource === null) {
             $this->openResource();
         }
+
+        $formatter = $this->formatter;
+        $line = $formatter($level, (string) $message, $context);
 
         fwrite($this->resource, $line . PHP_EOL); // @phpstan-ignore-line
     }
