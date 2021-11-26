bdba1493ec7e4ac06a496e364beae1963d07f696
Florent Poujol <florent.poujol@freshmile.com>
2021-11-26 23:50:21 +0100

    Cache: add test for the ArrayCache

--- src/Components/Cache/ArrayCache.php
+++ src/Components/Cache/ArrayCache.php
@@ -9,16 +9,11 @@ final class ArrayCache implements CacheInterface
     /** @var array<string, array{0: int, 1: mixed}> Each cache item is the expiration timestamp and value */
     private array $items = [];
 
-    public function __construct(
-        private string $prefix = '',
-    ) {
-    }
-
     public function set(string $key, mixed $value, int $ttlInSeconds = null): void
     {
         $expirationTimestamp = $ttlInSeconds === null ? PHP_INT_MAX : time() + $ttlInSeconds;
 
-        $this->items[$this->prefix . $key] = [$expirationTimestamp, $value];
+        $this->items[$key] = [$expirationTimestamp, $value];
     }
 
     public function increment(string $key, int $initialValue = 0, int $ttlInSeconds = null): int
@@ -45,14 +40,14 @@ final class ArrayCache implements CacheInterface
             return $initialValue;
         }
 
-        $this->items[$this->prefix . $key][1] += $offset;
+        $this->items[$key][1] += $offset;
 
-        return $this->items[$this->prefix . $key][1];
+        return $this->items[$key][1];
     }
 
     public function has(string $key): bool
     {
-        return $this->get($key) === null;
+        return $this->get($key) !== null;
     }
 
     public function keys(string $prefix = ''): array
@@ -62,11 +57,10 @@ final class ArrayCache implements CacheInterface
         }
 
         $keys = [];
-        $prefix = $this->prefix . $prefix;
         $currentTimestamp = time();
 
         foreach ($this->items as $key => $item) {
-            if ($item[0] >= $currentTimestamp && str_starts_with($key, $prefix)) {
+            if ($item[0] > $currentTimestamp && str_starts_with($key, $prefix)) {
                 $keys[] = $key;
             }
         }
@@ -76,14 +70,13 @@ final class ArrayCache implements CacheInterface
 
     public function get(string $key, mixed $default = null): mixed
     {
-        $key = $this->prefix . $key;
         $item = $this->items[$key] ?? null;
 
         if ($item === null) {
             return $default;
         }
 
-        if ($item[0] <= time()) {
+        if ($item[0] > time()) {
             return $item[1];
         }
 
@@ -94,7 +87,7 @@ final class ArrayCache implements CacheInterface
 
     public function delete(string $key): void
     {
-        unset($this->items[$this->prefix . $key]);
+        unset($this->items[$key]);
     }
 
     public function flush(string $prefix = ''): int
@@ -107,7 +100,6 @@ final class ArrayCache implements CacheInterface
         }
 
         $count = 0;
-        $prefix = $this->prefix . $prefix;
 
         foreach ($this->items as $key => $item) {
             if (str_starts_with($key, $prefix)) {
@@ -121,11 +113,10 @@ final class ArrayCache implements CacheInterface
 
     public function flushExpiredValues(): int
     {
-        $time = time();
         $count = 0;
 
         foreach ($this->items as $key => $item) {
-            if ($item[0] < $time) { // this assumes we go through all the items in less than 1 second
+            if ($item[0] <= time()) {
                 unset($this->items[$key]);
                 ++$count;
             }
---
+++ tests/Components/Cache/ArrayCacheTest.php
@@ -0,0 +1,141 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\FlorentPoujol\SmolFramework\Components\Cache;
+
+use FlorentPoujol\SmolFramework\Components\Cache\ArrayCache;
+use PHPUnit\Framework\TestCase;
+
+final class ArrayCacheTest extends TestCase
+{
+    public function test_main(): void
+    {
+        // arrange
+        $cache = new ArrayCache();
+
+        self::assertNull($cache->get('bool'));
+        self::assertNull($cache->get('string'));
+        self::assertNull($cache->get('int'));
+        self::assertNull($cache->get('array'));
+        self::assertNull($cache->get('object'));
+        self::assertNull($cache->get('whatever'));
+
+        self::assertEmpty($cache->keys());
+
+        // act
+        $cache->set('bool', true);
+        $cache->set('string', 'string');
+        $cache->set('int', 1);
+        $cache->set('array', ['some' => 'array']);
+        $object = new \stdClass();
+        $cache->set('object', $object);
+        $object->the_property = true;
+
+        // assert
+
+        // test has
+        self::assertTrue($cache->has('bool'));
+        self::assertTrue($cache->has('string'));
+        self::assertTrue($cache->has('int'));
+        self::assertTrue($cache->has('array'));
+        self::assertTrue($cache->has('object'));
+        self::assertFalse($cache->has('whatever'));
+
+        // test get
+        self::assertTrue($cache->get('bool'));
+        self::assertSame('string', $cache->get('string'));
+        self::assertSame(1, $cache->get('int'));
+        self::assertSame(['some' => 'array'], $cache->get('array'));
+        self::assertSame($object, $cache->get('object'));
+        self::assertSame($object->the_property, $cache->get('object')->the_property);
+        self::assertNull($cache->get('whatever'));
+
+        // test keys
+        $allKeys = ['bool', 'string', 'int', 'array', 'object'];
+        self::assertSame($allKeys, $cache->keys());
+        self::assertSame(['bool'], $cache->keys('bo'));
+
+        // test delete
+        $cache->delete('nothing');
+        $cache->delete('string');
+        self::assertNull($cache->get('string'));
+        self::assertFalse($cache->has('string'));
+        self::assertEmpty($cache->keys('stri'));
+
+        $cache->delete('array');
+        $allKeys = ['bool', 'int', 'object'];
+        self::assertSame($allKeys, $cache->keys());
+
+        // test increment/decrement
+        self::assertSame(1, $cache->get('int'));
+
+        self::assertSame(3, $cache->offsetInteger('int', 2));
+        self::assertSame(3, $cache->get('int'));
+
+        self::assertSame(4, $cache->increment('int'));
+        self::assertSame(4, $cache->get('int'));
+
+        self::assertSame(2, $cache->offsetInteger('int', -2));
+        self::assertSame(2, $cache->get('int'));
+
+        self::assertSame(1, $cache->decrement('int'));
+        self::assertSame(0, $cache->decrement('int'));
+        self::assertSame(0, $cache->get('int'));
+
+        // test flush
+        self::assertSame(0, $cache->flushExpiredValues());
+
+        self::assertTrue($cache->get('bool'));
+        self::assertNull($cache->get('string'));
+        self::assertSame(0, $cache->get('int'));
+        self::assertNull($cache->get('array'));
+        self::assertSame($object, $cache->get('object'));
+        self::assertNull($cache->get('whatever'));
+
+        self::assertSame(1, $cache->flush('obj'));
+
+        self::assertTrue($cache->get('bool'));
+        self::assertNull($cache->get('string'));
+        self::assertSame(0, $cache->get('int'));
+        self::assertNull($cache->get('array'));
+        self::assertNull($cache->get('object'));
+        self::assertNull($cache->get('whatever'));
+
+        self::assertSame(2, $cache->flush());
+
+        self::assertNull($cache->get('bool'));
+        self::assertNull($cache->get('string'));
+        self::assertNull($cache->get('int'));
+        self::assertNull($cache->get('array'));
+        self::assertNull($cache->get('object'));
+        self::assertNull($cache->get('whatever'));
+    }
+
+    public function test_ttl_expiration(): void
+    {
+        // arrange
+        $cache = new ArrayCache();
+
+        self::assertFalse($cache->has('exp'));
+        self::assertFalse($cache->has('no_exp'));
+
+        // act
+        $cache->set('exp', 'exp', 1);
+        $cache->set('no_exp', 'no_exp');
+
+        // assert
+
+        self::assertTrue($cache->has('exp'));
+        self::assertTrue($cache->has('no_exp'));
+        self::assertSame(['exp', 'no_exp'], $cache->keys());
+
+        sleep(1);
+
+        self::assertFalse($cache->has('exp'));
+        self::assertTrue($cache->has('no_exp'));
+        self::assertSame(['no_exp'], $cache->keys());
+
+        self::assertSame(0, $cache->flushExpiredValues()); // 0 because has() calls get() which unset the expired value
+    }
+}
